5b75f63dfae8b921056f702456ffc6dd6a2252e2
Lucas Zanella <me@lucaszanella.com>
2018-04-09 22:29:52 -0300

    small enhancements

--- socatController.js
+++ socatController.js
@@ -1,5 +1,5 @@
 const express     = require('express')
-var bodyParser    = require('body-parser')
+const bodyParser  = require('body-parser')
 const fs          = require('fs');
 const http        = require('http');
 const https       = require('https');
@@ -10,10 +10,10 @@ const { exec }    = require('child_process');
 const credentials = {key: privateKey, cert: certificate};
 const express     = require('express');
 const app         = express();
-//app.use(bodyParser.json());
+
 app.use(express.urlencoded());
-var   socatProcess;
 const token = tokenFile.token;
+var   socatProcess;
 
 if (token.length()==0)
 	throw Error('Token is empty, change token.js file');
@@ -21,7 +21,7 @@ else if (token.length() <= 12)
 	throw Error('Token is too small and can be bruteforced. Make it random and longer than 12 characters');
 
 listenOn: 8442;
-//TODO: Verify is these regex reall match only IP, nothing more
+//TODO: Be 100% sure that these regex really match only IP and ports, nothing more
 ipRegex   = /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/;
 portRegex = /^\d{1,5}$/;
 
@@ -29,19 +29,21 @@ redirect = (req, res) => {
 	const destinationIp    = req.body.destinationIp,
         const destinationPort  = req.body.destinationPort;
         const _token           = req.body.token;
+
 	if (token != _token) //Authentication
 		return;
 	if (!ipRegex.test(destinationIp) || !portRegex.test(destinationPort)) //Injection prevention
 		return;
-	if (!ipRegex.test(listenOn))
-		return;//this one is not really needed because it's passed by docker, not by nodejs, but there's no harm in doing this check, who knows...
+	if (!ipRegex.test(listenOn)) //Not really needed but who knows
+		return;
+
 	socatProcess.kill();
 	socatProcess = exec(`socat tcp-listen:${listenOn},bind=0.0.0.0,reuseaddr,fork tcp:${destinationIp}:${destinationPort}`);
+
 	res.send(`Started socat on docker's 0.0.0.0, redirecting tcp traffic from 0.0.0.0:${listenOn} to ${destinationIp}:${destinationPort}`)
 }
 
 app.get('/socat', redirect) 
 
 const httpsServer = https.createServer(credentials, app);
-
 httpsServer.listen(8442);
